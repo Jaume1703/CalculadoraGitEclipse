57be1e40d810e532eb6f78c977afc1016a0f1ab8
Jaume1703 <jaumelopez17@gmail.com>
2025-02-17 19:02:36 +0100

    a

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,10 +1,9 @@
 package calculadora;
 
 public class Calculadora {
-	 public int sumar(int a, int b) {
-	 return a + b;
-	 }
-	 public static void main(String[] args) {
-	 Calculadora calc = new Calculadora();
-	 System.out.println("Resultado de la suma: " +
-	calc.sumar(5, 3));}}
+	public int restar(int a, int b) {
+		 return a - b;
+		}
+		public int multiplicar(int a, int b) {
+		 return a * b;
+		}}
